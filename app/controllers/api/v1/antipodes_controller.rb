a7b38e7e9e902c9c0446e228c78136c148e843c7
Michael Dao <mike@turing.io>
2019-04-10 14:35:39 -0600

    adds debugging

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AntipodesController < ApplicationController
   def show
     if valid_key?
-    render json: AntipodeSerializer.new(Antipode.new(params[:lat], params[:long]))
+      render json: AntipodeSerializer.new(Antipode.new(params[:lat], params[:long]))
     else
       render body: "Unauthorized", status: 401
     end
@@ -14,6 +14,8 @@ class Api::V1::AntipodesController < ApplicationController
   end
 
   def valid_key?
+    puts request.env["API_KEY"]
+    puts ENV['api_key']
     request.env["API_KEY"] == ENV['api_key']
   end
 
